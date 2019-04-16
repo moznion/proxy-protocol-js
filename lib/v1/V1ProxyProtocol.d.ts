c3b06d74a479a97cdf9e60c11e13b818116a1204
moznion <moznion@gmail.com>
2019-04-16 16:23:43 +0900

    releng: v2.1.0

--- lib/v1/V1ProxyProtocol.d.ts
+++ lib/v1/V1ProxyProtocol.d.ts
@@ -4,9 +4,10 @@ export declare class V1ProxyProtocol {
     readonly inetProtocol: INETProtocol;
     readonly source: Peer;
     readonly destination: Peer;
+    readonly data?: string | undefined;
     private static readonly protocolSignature;
     private static readonly v1ProxyProtocolRegexp;
-    constructor(inetProtocol: INETProtocol, source: Peer, destination: Peer);
+    constructor(inetProtocol: INETProtocol, source: Peer, destination: Peer, data?: string | undefined);
     build(): string;
     static parse(input: string | Uint8Array): V1ProxyProtocol | null;
     static isValidProtocolSignature(input: string | Uint8Array): boolean;
